84983827b66e61fe010939ac7af95ec0da86135a
kailash chandra <37248387+kailashkai28@users.noreply.github.com>
2022-04-22 10:22:35 +0530

    Update ftpserver.cpp

--- ftpserver.cpp
+++ ftpserver.cpp
@@ -15,17 +15,48 @@ using namespace std;
 int i, j, tem;
 char buff[4096], t;
 FILE *f1;
-int main(int afg, char *argv[])
+int map1(string name,string pass)
+{
+	map<string,string> m1
+	{
+		{"kailash","kailash"},
+		{"kiran","Kiranunique"},
+		{"aadhish","aaaddd"}
+	};
+	
+	for(auto it= m1.begin();it!=m1.end();++it)
+	{
+	
+	if(user==it->first&&pass==it->second)
+		return 1;
+	else
+		return 0;
+	
+	}
+}
+
+int main(int argc, char *argv[])
 {
        int sockfd, newsockfd;
        socklen_t clength;
        struct sockaddr_in serv_addr,cli_addr;
-       char t[MAX], str[MAX];
-       strcpy(t,"exit");
+       char str[MAX];
+       if(argc!=2)
+       {
+		perror("Usage: ./a.out <port number>");
+		exit(1);
+       }
+       strcpy(t,"bye");
        sockfd=socket(AF_INET, SOCK_STREAM,0);
        serv_addr.sin_family=AF_INET;
        serv_addr.sin_addr.s_addr=inet_addr("127.0.0.1");
        serv_addr.sin_port=htons(SERV_TCP_PORT);
+       if(atoi(argv[1])!=8021)
+       {
+		perror("Please enter the correct port(must be 8021)");
+		exit(1);
+       }
+ 
        cout<<"\n socket with  the local server.......";
        bind(sockfd,(struct sockaddr*)&serv_addr,sizeof(serv_addr));
        cout<<"\naccept client connection request...";
