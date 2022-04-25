66012a216bb955daf08010bf10c72c4b4407e075
kailash chandra <37248387+kailashkai28@users.noreply.github.com>
2022-04-25 15:36:07 +0530

    client

--- client.cpp
+++ client.cpp
@@ -6,6 +6,45 @@
 #include<unistd.h>
 #include "include/userauth.h"
 using namespace std;
+void authen()
+{
+{
+	map<string,string> m = {{"kailash","chandra"},{"lash","chand"},{"Guru","dra"},{"anonymous",""}};
+	int loginAttempt = 0;
+	bool uservalid=false;
+   	string userName,userPassword;
+	map<string,string> :: iterator it;
+	do{
+		  cout<<"Enter userName:";
+          	  cin>>userName;
+               	  cout<<"Enter userPassword:";
+          	  cin>>userPassword;
+	for(it=m.begin();it!=m.end();++it)
+    	{	if(userName ==  it->first && userPassword == it->second)
+        	{
+            	cout<<"Welcome "<<userName<<"\n";
+		uservalid=true;
+        	break;
+		}
+	}
+	if(!uservalid)
+	{
+		loginAttempt++;
+		cout<<(3-loginAttempt)<<" attempts left.... "<<endl;
+    		cout << "Invalid login attempt. Please try again.\n";
+		if(loginAttempt==3)
+		{
+			cout<<"Attempts completed!!!"<<endl;
+		}
+	}
+	
+	}while(!uservalid&&loginAttempt<3);
+
+    //cout << "Thank you for logging in.\n";
+	
+}
+	
+
 int main(int argc,char* argv[])
 {
 	int sockfd=socket(AF_INET,SOCK_STREAM,0);
