d64ce85db559ed6f42c17757104dad7cdb350acf
kailash chandra <37248387+kailashkai28@users.noreply.github.com>
2022-04-28 02:33:16 +0530

    Add files via upload

--- userauth.h
+++ userauth.h
@@ -1,28 +1,159 @@
-#include<string.h>
 #include<iostream>
+#include<netinet/in.h>
+#include<arpa/inet.h>
+#include<string.h>
+#include<fstream>
+#include<stdio.h>
+#include<unistd.h>
+#include<stdlib.h>
+#include<errno.h>
+#include<sys/stat.h>
+#include<sys/types.h>
+#include<syslog.h>
+#include<fcntl.h>
+#include<signal.h>
+#include<sys/wait.h>
 #include<map>
 using namespace std;
-void authen()
+//bool authen(string,string);
+static void skeleton_daemon();
+bool isuser(string);
+void registr();
+void attempt();
+
+//bool loginValidate(string,string);
+
+//Kailash----------
+static void skeleton_daemon()
 {
-	map<string,string> m = {{"kailash","chandra"},{"lash","chand"},{"Guru","dra"},{"anonymous",""}};
+	pid_t pid,sid;
+	pid=fork();
+	if(pid<0)
+	{
+		exit(EXIT_FAILURE);
+	}
+	if(pid>0)
+	{
+		exit(EXIT_SUCCESS);
+	}
+	sid=setsid();
+	if(sid<0)
+	{
+		exit(EXIT_FAILURE);
+	}
+	signal(SIGCHLD,SIG_IGN);
+	signal(SIGHUP,SIG_IGN);
+
+	pid=fork();
+	if(pid<0)
+	{
+		exit(EXIT_FAILURE);
+	}
+	if(pid>0)
+	{
+		exit(EXIT_SUCCESS);
+	}
+
+	umask(0);
+
+	chdir("/");
+	int x;
+	for(x=sysconf(_SC_OPEN_MAX);x>=0;x--)
+	{
+		close(x);
+	}
+	openlog("firstdaemon",LOG_PID,LOG_DAEMON);
+}
+
+bool isuser(string uname)
+{
+	ifstream fin;
+	fin.open("users.txt");
+	if(!fin)
+	{
+		cerr<<"Error in opening file"<<endl;
+		exit(1);
+	}
+	else
+	{
+	string name,pass;
+
+		getline(fin,name);
+		//fin>>name;
+		if(name==uname)
+		{
+			string p;
+			cout<<"Enter password";
+			cin>>p;
+			getline(fin,pass);
+			//cout<<pass;
+			if(p==pass)
+			{//fin>>pass;
+			cout<<"Authentication successful\nWelcome "<<uname;
+			
+			}
+			else
+			{
+				int count=2;
+				while(count>=0)
+				{	
+					cout<<"Attempt no. "<<3-(count+1)<<endl;	
+					attempt();
+					count--;
+				}
+				if(count==0)
+				{
+					cout<<"Attempt exceeded......Exiting"<<endl;
+				}
+			}	
+		}
+	
+	else
+	{
+		registr();	
+		fin.close();	
+	}
+}
+}
+
+void attempt()
+{
+					string p;
+					cout<<"\nEnter the password again:";
+					cin>>p;
+						
+						
+}
+/*
+bool authen(string name,string pass)
+{
 	int loginAttempt = 0;
 	bool uservalid=false;
     string userName,userPassword;
-    //int flag=0;
-	map<string,string> :: iterator it;
+	ifstream fin;
+	fin.open("users.txt"); 	
+	while (!fin.eof())) 
+	{
+    		if()// input them into the map 
+	}
+       
 	do{
-		 cout<<"Enter userName:";
-          cin>>userName;
-          cout<<"Enter userPassword:";
-          cin>>userPassword;
-	for(it=m.begin();it!=m.end();++it)
-   	{	if(userName ==  it->first && userPassword == it->second)
+		 // cout<<"Enter userName:";
+          //cin>>userName;
+		for(it=m.begin();it!=m.end();++it)
+   		{	if(userName ==  name && userPassword == pass)
         	{
 		
             	cout<<"Welcome "<<userName<<"\n";
 				uservalid=true;
-				//flag=1;		
+				
+				return true;	
         		break;
+			}
+			else
+			{
+				cerr<<"Invalid user"<<endl;
+				break;
 			}
 	}
 	if(!uservalid)
@@ -33,12 +164,57 @@ void authen()
 		if(loginAttempt==3)
 		{
 			cout<<"Attempts completed!!!"<<endl;
-			//flag=0;
+			return false;
 		}
 	}
 	
 	}while(!uservalid&&loginAttempt<3);
-	//return flag;
+	fin.close();
     //cout << "Thank you for logging in.\n";
 	
 }
+*/
+
+void registr()
+{
+		string reguser,regpass;
+		cout<<"===NEW USER REGISTRATION==="<<endl;
+	    cout<<"Enter username:";
+	    cin>>reguser;
+	    cout<<"Enter password:";
+	    cin>>regpass;
+		ofstream fout;
+		fout.open("users.txt",ios::app);
+        fout<<reguser<<endl<<regpass<<endl;//' '<<regname<<' '<<regmobile<<' '<<regemail<<' '<<regaddress<< 
+      	fout.close();	
+}
+
+
+
+
+/*bool loginValidate(string user,string pass)
+{
+     int count;
+        string u,p;
+        ifstream fin("users.txt");
+       	while(fin>>u>>p)
+        {
+                if(u==user && p==pass)
+                {
+                        count=1;
+
+                }
+        }
+        fin.close();
+        if(count==1)
+        {
+            
+                return true;
+
+        }
+        else
+        {
+          	return false;
+        }
+}
+*/
